c4645e94444508696623ceb848bb33f63f6f2fe2
tomeichlersmith <eichl008@umn.edu>
2023-11-01 15:09:31 -0500

    refactor dimuon.py for more documentability

--- ana/dimuon.py
+++ ana/dimuon.py
@@ -16,6 +16,64 @@ import vector
 # to get the lorentz behavior we want
 vector.register_awkward()
 
+
+def _create_subbranch(tree, name, single = True):
+    """Create a local Callable object which can be used to access subbranches
+    of the input branch 'name'
+
+    Parameters
+    ----------
+    tree : uproot's TTree wrapper
+        tree we are reading from
+    name : str
+        branch we want to get subbranches for
+    single: bool, optional
+        if this branch is a single instance of an object per event or a collection
+    
+
+    Returns
+    -------
+    Callable
+        a function that can be called with a member name to retrieve an array
+        of that member
+    """
+    if single:
+        def _subbranch(member_name):
+            return tree[f'{name}/{member_name}'].array()
+        return _subbranch
+
+    def _subbranch(member_name):
+        return tree[f'{name}/{name}.{member_name}'].array()
+    return _subbranch
+
+
+def _particle(subbranch):
+    """get a particle formatting using the input subbranch function
+
+    Parameter
+    ---------
+    subbranch: Callable
+        function we can use to get the members of a Particle class
+    """
+    d = {
+        member : subbranch(member)
+        for member in [
+            'valid', 'track_id', 'pdg_id'
+        ]
+    }
+    d.update({
+        'momentum' : ak.zip({
+            c : subbranch(c)
+            for c in ['px','py','pz','energy']
+        }, with_name = 'Momentum4D'),
+        'position' : ak.zip({
+            c : subbranch(c)
+            for c in ['x','y','z','t']
+        }, with_name = 'Vector4D'),
+    })
+    return ak.zip(d, with_name='Particle')
+
+
 def loadup(fp):
     """Main loading function for dimuon analysis
 
@@ -28,50 +86,20 @@ def loadup(fp):
     fp : str, pathlib.Path
         file path to file we want to open and read
     """
+
+    with uproot.open(fp) as f:
+        t = f['dimuon_events']
     
-    t = uproot.open(f'{fp}:dimuon_events')
-    
-    def _create_subbranch(name, single):
-        if single:
-            def _subbranch(b):
-                return t[f'{name}/{b}'].array()
-            return _subbranch
-    
-        def _subbranch(b):
-            return t[f'{name}/{name}.{b}'].array()
-        return _subbranch
-    
-    
-    def particle(name, single = True):
-        subbranch = _create_subbranch(name, single)
         d = {
-            b : subbranch(b)
-            for b in [
-                'valid', 'track_id', 'pdg_id'
+            name : _particle(_create_subbranch(name))
+            for name in [
+                'incident', 'parent', 'mu_plus', 'mu_minus'
             ]
         }
         d.update({
-            'momentum' : ak.zip({
-                c : subbranch(c)
-                for c in ['px','py','pz','energy']
-            }, with_name = 'Momentum4D'),
-            'position' : ak.zip({
-                c : subbranch(c)
-                for c in ['x','y','z','t']
-            }, with_name = 'Vector4D'),
+            'ntries' : t['ntries'].array(),
+            'weight' : t['weight'].array(),
+            'extra' : _particle(_create_subbranch('extra', single=False)),
+            'ecal' : _particle(_create_subbranch('ecal', single=False))
         })
-        return ak.zip(d, with_name='Particle')
-    
-    d = {
-        name : particle(name)
-        for name in [
-            'incident', 'parent', 'mu_plus', 'mu_minus'
-        ]
-    }
-    d.update({
-        'ntries' : t['ntries'].array(),
-        'weight' : t['weight'].array(),
-        'extra' : particle('extra', single=False),
-        'ecal' : particle('ecal', single=False)
-    })
-    return ak.zip(d, depth_limit=1)
+        return ak.zip(d, depth_limit=1)
