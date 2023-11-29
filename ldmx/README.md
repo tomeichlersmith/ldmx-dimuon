b73842455d8803ca156f12c86364324501a6b1cb
tomeichlersmith <eichl008@umn.edu>
2023-11-29 14:16:44 -0600

    initialize a new directory focused on running within ldmx-sw

---
+++ ldmx/README.md
@@ -0,0 +1,19 @@
+# Di-Muon Calibration within ldmx-sw
+In order to begin di-muon calibration studies, we want to first look at
+how dimuon events present themselves within the LDMX detector as designed.
+
+This directory is focused on this study: using ldmx-sw to study dimuon events.
+
+### Table of Contents
+- `target_mumu.py`: ldmx-sw configuration script used to generate simulated data
+- `requirements.txt`: list of primary requirements and their pinned versions for python-based analysis
+
+### Start Up
+In whatever environment you choose, we should use the same versions of python packages
+so the analysis code can be ensured to return the same results and behave in the same
+way.
+```
+pip install -r requirements.txt
+```
+If you see a "version not available" error, that means you are probably using a python
+version that is too old to use the versions listed in the requirements file.
---
+++ ldmx/target_mumu.py
@@ -0,0 +1,69 @@
+import sys
+import argparse
+
+parser = argparse.ArgumentParser(
+    f'ldmx fire {sys.argv[0]}',
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter
+)
+
+parser.add_argument(
+    'run_number', default=1,
+    help='set the random seed for the simulation',
+    type=int
+)
+
+arg = parser.parse_args()
+
+from LDMX.Framework import ldmxcfg
+
+p = ldmxcfg.Process('dimuon')
+p.maxTriesPerEvent = 1
+p.maxEvents = 1000000
+p.termLogLevel = 1
+p.logFrequency = p.maxEvents // 100
+p.run = arg.run_number
+
+from LDMX.SimCore import simulator
+from LDMX.SimCore import generators
+from LDMX.SimCore import bias_operators
+from LDMX.Biasing import filters
+from LDMX.Biasing import util
+sim = simulator.simulator('target_dimuon')
+sim.setDetector('ldmx-det-v14-8gev', True)
+sim.description = 'gamma conversion to muons in the target'
+sim.beamSpotSmear = [20., 80., 0.]
+sim.generators = [ generators.single_8gev_e_upstream_tagger() ]
+
+beam = sim.generators[0].energy*1000
+tagger = 0.95*beam
+minbrem = 0.625*beam
+
+
+sim.biasing_operators = [ bias_operators.GammaToMuPair('target', 1e6, minbrem) ]
+sim.actions = [
+    filters.TaggerVetoFilter(thresh = tagger),
+    filters.TargetBremFilter(recoil_max_p = beam - minbrem, brem_min_e = minbrem),
+    filters.TargetGammaMuMuFilter(),
+    util.TrackProcessFilter.gamma_mumu()
+]
+
+params = [
+    'beam', '8gev',
+    'tagger', str(int(tagger)),
+    'minbrem', str(int(minbrem)),
+    'run', f'{p.run:04d}'
+]
+p.outputFiles = [
+  '_'.join(params)+'.root'
+]
+
+import LDMX.Ecal.EcalGeometry
+import LDMX.Hcal.HcalGeometry
+import LDMX.Ecal.ecal_hardcoded_conditions
+import LDMX.Ecal.digi as ecal_digi
+
+p.sequence = [
+    sim,
+    ecal_digi.EcalDigiProducer(),
+    ecal_digi.EcalRecProducer()
+    ]
