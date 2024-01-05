1449622f56c7f357baa07479a1b2ce2ad255554e
tomeichlersmith <eichl008@umn.edu>
2024-01-05 15:55:32 -0600

    update reader to include sim hits and sim particles
    - generalize loader to accept branch names and formatters
    - write formatters for sim hits and sim particles
    - include a parent mixin for ecal det id translation
    - include empty mixins for other classes to leave space for impl

--- ldmx/upldmx.py
+++ ldmx/upldmx.py
@@ -4,20 +4,105 @@ import uproot
 import numpy as np
 import awkward as ak
 
+import vector
+vector.register_awkward()
+
 @ak.mixin_class(ak.behavior)
-class EcalRecHit:
-    """awkward behavior mixin adding functionality to EcalRecHit data"""
+class WithEcalDetID:
+    @property
     def layer(self):
-        """deduce the layer a hit is in from its detector ID"""
         return (self.id >> 17) & 0x3f
 
-def load_rec_hits(
+
+@ak.mixin_class(ak.behavior)
+class EcalRecHit(WithEcalDetID):
+    """awkward behavior mixin adding functionality to EcalRecHit data"""
+    pass
+
+
+def as_rec_hit(events, branch):
+    return ak.zip({
+        m : events[f'{branch}.{m}_']
+        for m in [
+            'id', 'amplitude', 'energy', 'time',
+            'xpos', 'ypos', 'zpos', 'isNoise'
+        ]
+    }, with_name = EcalRecHit.__name__)
+
+
+@ak.mixin_class(ak.behavior)
+class SimCalHit(WithEcalDetID):
+    def has_contrib(self, pdg):
+        return ak.any(self.contrib.pdgCode==pdg, axis=-1)
+    def ncontribs(self):
+        return ak.count(self.contib.pdgCode, axis=-1)
+
+def as_sim_hit(events, branch):
+    form = {
+        m : events[f'{branch}.{m}_']
+        for m in [
+            'id', 'edep'
+        ]
+    }
+    form.update({
+        'pos' : ak.zip({
+            c : events[f'{branch}.{c}_']
+            for c in ['x','y','z','time']
+        }, with_name='Vector4D'),
+        'contrib' : ak.zip({
+            m : events[f'{branch}.{m}Contribs_']
+            for m in [
+                'trackID', 'incidentID', 'pdgCode', 'edep', 'time'
+            ]
+        }, with_name='SimCalHitContrib')
+    })
+    return ak.zip(form, depth_limit = 2, with_name = SimCalHit.__name__)
+
+
+@ak.mixin_class(ak.behavior)
+class SimParticle:
+    pass
+
+
+def as_sim_particle(events, branch):
+    form = {
+        m : events[f'{branch}.second.{m}_']
+        for m in [
+            'pdgID', 'genStatus', 'mass', 'charge', 'daughters', 'parents',
+            'processType', 'vertexVolume'
+        ]
+    }
+    form.update({
+        'track_id' : events[f'{branch}.first'],
+        'momentum' : ak.zip({
+            c : events[f'{branch}.second.{c}_']
+            for c in ['px','py','pz','energy']
+        }, with_name='Momentum4D'),
+        'vertex' : ak.zip({
+            c : events[f'{branch}.second.{c}_']
+            for c in ['x','y','z','time']
+        }, with_name='Vector4D'),
+        'end_vertex' : ak.zip({
+            c : events[f'{branch}.second.end{c.upper()}_']
+            for c in ['x','y','z']
+        }, with_name='Vector3D'),
+    })
+    return ak.zip(form, depth_limit=2, with_name = SimParticle.__name__)
+
+
+def arrays(
     fp = None, *,
     include_header_params = False,
+    branches = {
+        'EcalRecHits_dimuon' : as_rec_hit,
+        'EcalSimHits_dimuon' : as_sim_hit,
+        'SimParticles_dimuon' : as_sim_particle
+    },
+    remove_pass = True
 ):
-    """load ecal rec hits and event header into memory for analysis
+    """load various branches into awkward arrays in memory
 
-    Parameters
+        Parameters
     ----------
     fp : str, optional
         path to file(s) to load
@@ -27,6 +112,12 @@ def load_rec_hits(
         load the *Parameters from the EventHeader
         the code for doing this is kinda complicated so I want to keep it
         around but I don't think these are necessary for this study
+    branches: dict, optional
+        branch name to formatter dictionary
+        default includes ecal rec hits, ecal sim hits, and sim particles
+    remove_pass: bool, optional
+        whether to remove passname from branch names upon formatting
+        default is True (yes, do that)
     """
     
     if fp is None:
@@ -35,18 +126,12 @@ def load_rec_hits(
     # load the data we want from the file into memory
     events = uproot.concatenate(
         fp+':LDMX_Events',
-        filter_name = [ 'EventHeader**', 'EcalRecHits**' ]
+        filter_name = ['EventHeader**']+[br+'**' for br in branches]
     )
 
     # reformat the data into a more helpful form
-    #  (basically just making shorter names and attaching helper functions)
-    rec_hit_form = {
-        m : events[f'EcalRecHits_dimuon.{m}_']
-        for m in [
-            'id', 'amplitude', 'energy', 'time', 'xpos','ypos','zpos','isNoise'
-        ]
-    }
-    header_form = {
+    #   (shorter names and such)
+    form = {
         name : events[branch]
         for name, branch in [
             ('number', 'eventNumber_'),
@@ -54,9 +139,28 @@ def load_rec_hits(
             ('weight', 'weight_')
         ]
     }
-    header_form['EcalRecHits'] = ak.zip(rec_hit_form, with_name=EcalRecHit.__name__)
     if include_header_params:
         for param_type in ['float','int','string']:
             for key in np.unique(ak.flatten(events[f'{param_type}Parameters_.first']).to_numpy()):
                 header_form[key] = ak.flatten(events[f'{param_type}Parameters_.second'][events[f'{param_type}Parameters_.first']==key])
-    return ak.zip(header_form, depth_limit=1)
+    for branch, formatter in branches.items():
+        name = branch
+        if remove_pass:
+            name = name[:name.index('_')]
+        form[name]  = formatter(events, branch)
+    return ak.zip(form, depth_limit=1)
+
+
+def load_rec_hits(fp = None, **kwargs):
+    """load ecal rec hits and event header into memory for analysis
+
+    Parameters
+    ----------
+    fp : str, optional
+        path to file(s) to load
+        the default is to load all files that Tom generated and put in
+        a specific directory in the shared group space
+    kwargs: dict, optional
+        key word arguments passed to arrays
+    """
+    return arrays(fp, branches = {'EcalRecHits_dimuon':as_rec_hit}, **kwargs)
